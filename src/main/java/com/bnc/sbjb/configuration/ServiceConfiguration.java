9b9c10f3f8df56efb118efe0679ffddf407b06de
Bhavik Kumar <bhavik@bravenewcoin.com>
2018-07-22 20:24:45 +1200

    Add hostname to metrics

--- src/main/java/com/bnc/sbjb/configuration/ServiceConfiguration.java
+++ src/main/java/com/bnc/sbjb/configuration/ServiceConfiguration.java
@@ -1,19 +1,39 @@
 package com.bnc.sbjb.configuration;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.beans.factory.annotation.Value;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class ServiceConfiguration {
 
-    @Value("${spring.application.name:unknown}")
-    private String applicationName;
+    private final BuildProperties buildProperties;
+
+    private final Environment environment;
+
+    @Autowired
+    public ServiceConfiguration(final BuildProperties buildProperties, final Environment environment) {
+        this.buildProperties = buildProperties;
+        this.environment = environment;
+    }
 
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-        return registry -> registry.config().commonTags("name", applicationName);
+        return registry -> {
+            try {
+                registry.config()
+                    .commonTags("application", buildProperties.getName(), "container",
+                        InetAddress.getLocalHost().getHostName());
+            } catch (UnknownHostException e) {
+                registry.config()
+                    .commonTags("application", buildProperties.getName(), "container", "unknown");
+            }
+        };
     }
 }
