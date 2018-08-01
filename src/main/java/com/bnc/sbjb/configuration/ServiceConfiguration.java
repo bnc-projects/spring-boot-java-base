a0ef280b29613a77bd0bb2e3fe86c653c314d49b
Bhavik Kumar <bhavik@bravenewcoin.com>
2018-08-01 21:51:18 +1200

    Change the bean to conditional for testing purposes

--- src/main/java/com/bnc/sbjb/configuration/ServiceConfiguration.java
+++ src/main/java/com/bnc/sbjb/configuration/ServiceConfiguration.java
@@ -5,6 +5,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,15 +13,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ServiceConfiguration {
 
-    private final BuildProperties buildProperties;
-
-    @Autowired
-    public ServiceConfiguration(final BuildProperties buildProperties) {
-        this.buildProperties = buildProperties;
-    }
-
     @Bean
-    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+    @ConditionalOnSingleCandidate(BuildProperties.class)
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(BuildProperties buildProperties) {
         return registry -> {
             try {
                 registry.config()
