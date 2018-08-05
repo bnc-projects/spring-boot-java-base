c01e5a0d41f5ab2884034a1113bbb823864bfccc
Bhavik Kumar <bhavikk85@gmail.com>
2018-08-05 15:44:33 +1200

    Remove build properties bean

--- src/main/java/com/bnc/sbjb/configuration/MetricsConfiguration.java
+++ src/main/java/com/bnc/sbjb/configuration/MetricsConfiguration.java
@@ -5,8 +5,8 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,16 +16,13 @@ public class MetricsConfiguration {
     private static final Logger logger = LoggerFactory.getLogger(MetricsConfiguration.class);
 
     @Bean
-    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(BuildProperties buildProperties) {
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(@Value("${spring.application.name") String name) {
         return registry -> {
             try {
-                registry.config()
-                    .commonTags("application", buildProperties.getName(), "container",
-                        InetAddress.getLocalHost().getHostName());
+                registry.config().commonTags("application", name, "container", InetAddress.getLocalHost().getHostName());
             } catch (UnknownHostException ex) {
                 logger.warn("Could not get hostname for metrics");
-                registry.config()
-                    .commonTags("application", buildProperties.getName());
+                registry.config().commonTags("application", name, "container", "unknown");
             }
         };
     }
