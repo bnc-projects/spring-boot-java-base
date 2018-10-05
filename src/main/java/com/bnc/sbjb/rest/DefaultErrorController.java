5d947603d09613fc691af3949671ebaf745afa24
Bhavik Kumar <bhavikk85@gmail.com>
2018-10-05 16:56:53 +1300

    Simplify deployment by using python script (#27)
    * Update dependencies
    * Upgrade gradle wrapper to 4.10.2
    * Upgrade checkstyle to version 8.1
    * Fix up code smells
    * Simplify deployment by using python script
    * Update error controller to respond with 404 and no body

--- src/main/java/com/bnc/sbjb/rest/DefaultErrorController.java
+++ src/main/java/com/bnc/sbjb/rest/DefaultErrorController.java
@@ -1,9 +1,8 @@
 package com.bnc.sbjb.rest;
 
-import com.bnc.sbjb.model.api.CustomError;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +12,8 @@ public class DefaultErrorController implements ErrorController {
     private static final String PATH = "/error";
 
     @RequestMapping(value = PATH)
-    public CustomError error(HttpServletRequest request) {
-        return new CustomError(HttpStatus.NOT_FOUND, "Not Found");
+    public ResponseEntity<String> error() {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @Override
