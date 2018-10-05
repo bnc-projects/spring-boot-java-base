86979d59b0a83038418406c409ba7dc99379e5a2
Bhavik Kumar <bhavik@bravenewcoin.com>
2018-10-05 15:12:31 +1300

    Fix broken test

--- src/test/java/com/bnc/sbjb/rest/DefaultErrorControllerTest.java
+++ src/test/java/com/bnc/sbjb/rest/DefaultErrorControllerTest.java
@@ -2,7 +2,6 @@ package com.bnc.sbjb.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.bnc.sbjb.model.api.CustomError;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -24,8 +24,8 @@ class DefaultErrorControllerTest {
 
     @Test
     void error() {
-        CustomError actual = template.getForObject("/invalidurl", CustomError.class);
-        assertThat(actual.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+        ResponseEntity<String> actual = template.getForEntity("/invalidurl", String.class);
+        assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
     @Test
