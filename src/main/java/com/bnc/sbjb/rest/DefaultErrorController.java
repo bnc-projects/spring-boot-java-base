2d6dc8e12a7a5763c0c3dd8e306b89c69b41b991
Bhavik Kumar <bhavik@bravenewcoin.com>
2018-10-05 16:35:13 +1300

    Fix up code smells

--- src/main/java/com/bnc/sbjb/rest/DefaultErrorController.java
+++ src/main/java/com/bnc/sbjb/rest/DefaultErrorController.java
@@ -2,8 +2,8 @@ package com.bnc.sbjb.rest;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +12,8 @@ public class DefaultErrorController implements ErrorController {
     private static final String PATH = "/error";
 
     @RequestMapping(value = PATH)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public void error() {
+    public ResponseEntity<String> error() {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @Override
