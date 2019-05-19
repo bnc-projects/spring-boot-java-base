0d93ed60d0f710470397722862444ff112e623e0
Yi Zhang <yi@bravenewcoin.com>
2019-05-20 11:14:05 +1200

    Remove cloudformation related templates and scripts

--- deployment/scripts/common/push-docker-image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Login into ECR ..."
-
-$(aws ecr get-login --no-include-email)
-
-echo "Push docker image to ECR ..."
-
-./gradlew docker dockerTag dockerPush -PTAG=$TRAVIS_BUILD_NUMBER -PREPOSITORY_URI=$REPOSITORY_URI
