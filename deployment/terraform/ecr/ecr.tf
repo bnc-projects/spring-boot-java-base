7469430ab7521577a2cc85ed07e76905382a5f34
Bhavik Kumar <bhavikk85@gmail.com>
2019-05-21 11:49:39 +1200

    Revert "Revert "Reference BNC operations tf project""
    This reverts commit a208fe7d6603eab81f096b5d8ce0e5bacff5927e.

--- deployment/terraform/ecr/ecr.tf
+++ deployment/terraform/ecr/ecr.tf
@@ -1,11 +1,11 @@
 module "ecr" {
   source                   = "git::https://github.com/bnc-projects/terraform-aws-ecr.git?ref=1.0.0"
   allowed_read_principals  = [
-    "${data.terraform_remote_state.techemy.bnc_account_ids["market_data_development"]}",
-    "${data.terraform_remote_state.techemy.bnc_account_ids["market_data_production"]}",
+    "${data.terraform_remote_state.bnc_ops.bnc_account_ids["market_data_development"]}",
+    "${data.terraform_remote_state.bnc_ops.bnc_account_ids["market_data_production"]}",
   ]
   allowed_write_principals = [
-    "${data.terraform_remote_state.techemy.bnc_deployment_roles["operations"]}",
+    "${data.terraform_remote_state.bnc_ops.deployment_role_arn}"
   ]
   ecr_repo_name            = "${var.service_name}"
   tags                     = "${merge(local.common_tags, var.tags)}"
