2b50025feff334e1078d10ddfb14e8c9df600011
Bhavik Kumar <bhavikk85@gmail.com>
2019-05-21 11:05:22 +1200

    Only run terraform output once

--- README.md
+++ README.md
@@ -70,7 +70,7 @@ ROLE_ARN=
 STATE_S3_BUCKET=
 STATE_DYNAMODB_TABLE=
 KEY=<The project key for the ECR repository>, e.g bnc/<team>/ecr/<service-name>
-SERVICE_KEY=<The project key for ECS service>, e.g bnc/<team>/<workspace>/ecs/<service-name>
+SERVICE_KEY=<The project key for ECS service>, e.g ecs/<service-name>
 SPLUNK_URL=
 ```
 
--- deployment/terraform/ecs-service/outputs.tf
+++ deployment/terraform/ecs-service/outputs.tf
@@ -1,8 +1,8 @@
+output "deployment_role_arn" {
+  value = "${data.terraform_remote_state.techemy.bnc_deployment_roles["market_data_${terraform.workspace}"]}"
+}
+
 output "ecs_cluster_name" {
   sensitive = true
   value     = "${data.terraform_remote_state.market-data.ecs_cluster_name}"
-}
-
-output "deployment_role_arn" {
-  value = "${data.terraform_remote_state.techemy.bnc_deployment_roles["market_data_${terraform.workspace}"]}"
 }
