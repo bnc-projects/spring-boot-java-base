6d906df86386a2f6830a4389fa24331064e57a7d
Bhavik Kumar <bhavik@bravenewcoin.com>
2019-06-04 09:41:00 +1200

    Update deployment and dependencies (#51)

--- deployment/terraform/ecr/outputs.tf
+++ deployment/terraform/ecr/outputs.tf
@@ -1,8 +1,3 @@
 output "repository_url" {
   value = module.ecr.repository_url
 }
-
-output "deployment_role_arn" {
-  sensitive = true
-  value     = data.terraform_remote_state.bnc_ops.outputs.deployment_role_arn
-}
