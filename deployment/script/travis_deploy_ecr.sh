9de7018e3067220ff8ab2f50557f296f64a56f63
Bhavik Kumar <bhavik@bravenewcoin.com>
2020-01-28 14:26:41 +1300

    Move repository uri environment variable to ecr script

--- deployment/script/travis_deploy_ecr.sh
+++ deployment/script/travis_deploy_ecr.sh
@@ -3,3 +3,4 @@
 cd deployment/terraform/ecr
 terraform init -backend-config="bucket=${STATE_S3_BUCKET}" -backend-config="region=${AWS_DEFAULT_REGION}" -backend-config="dynamodb_table=${STATE_DYNAMODB_TABLE}" -backend-config="kms_key_id=${KMS_KEY_ID}" -backend-config="key=${KEY}" -backend-config="role_arn=${ROLE_ARN}" 1>/dev/null || exit 1
 terraform apply -backup="-" -input=false -auto-approve -var role_arn=${ROLE_ARN} -var service_name=${SERVICE_NAME} -var tags="{\"AuthorName\":\"${AUTHOR_NAME}\",\"GitRepository\":\"${TRAVIS_REPO_SLUG}\"}" 1>/dev/null || exit 1
+export REPOSITORY_URI=$(terraform output repository_url)
